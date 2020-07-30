d8026f35cfb4ffccd897d401a37cc4751d38f5f2
AbisaiSan <abisaiweb7@gmail.com>
2020-07-30 11:31:48 -0300

    Criando area de membros

--- controllers/members.js
+++ controllers/members.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-const data = require('./data.json')
-const {age, date} = require('./utils')
+const data = require('../data.json')
+const {age, date} = require('../utils')
 
 
 //INDEX
@@ -8,34 +8,33 @@ exports.index = function(req, res){
 
 
 
-  return res.render("instructors/index", {instructors: data.instructors})
+  return res.render("members/index", {members: data.members})
 }
 
 //SHOWS
 exports.show = function(req, res){
   const {id} = req.params
 
-  const foundInstructor = data.instructors.find(function(instructor){
-    return instructor.id == id
+  const foundMember = data.members.find(function(member){
+    return member.id == id
   })
-  if(!foundInstructor ) return res.send("instrutor não existe")
+  if(!foundMember ) return res.send("Membro não existe")
 
-  const instructor = {
-    ...foundInstructor,
-    age: age(foundInstructor.birth),
-    services:foundInstructor.services.split(","),
-    created_at: new Intl.DateTimeFormat("en-GB").format(foundInstructor.created_at)
+  const member = {
+    ...foundMember,
+    age: age(foundMember.birth)
   }
 
-  return res.render("instructors/show", {instructor})
+  return res.render("members/show", {member})
 }
 
-
-
-
 //Create
-//Validação de dados
+exports.create = function(req, res){
+  return res.render("members/create")
+}
 
+//Post
+//Validação de dados
 exports.post = function(req, res){
 
   const keys = Object.keys(req.body)
@@ -61,10 +60,10 @@ exports.post = function(req, res){
   //Pega a data do momento em que esta sendo salvo
   const created_at = Date.now()
 
-  const id = Number(data.instructors.length + 1)
+  const id = Number(data.members.length + 1)
   //Criando uma chave dentro do arquivo data.json
   //E passando os dados do req.body 
-  data.instructors.push({
+  data.members.push({
     id, 
     avatar_url, 
     name, 
@@ -78,87 +77,82 @@ exports.post = function(req, res){
 
   fs.writeFile("data.json", JSON.stringify(data, null,4), function(err){
       if(err) return res.send("write fail erro") 
-        return res.redirect('/instructors')
+        return res.redirect('/members')
       
   } )
 
   //return res.send(req.body)
 }
 
-
 //Edit = apenas página para editar
-
 exports.edit = function(req, res){
 
   const {id} = req.params
 
-  const foundInstructor = data.instructors.find(function(instructor){
-    return instructor.id == id
+  const foundMember = data.members.find(function(member){
+    return member.id == id
   })
-  if(!foundInstructor ) return res.send("instrutor não existe")
+  if(!foundMember ) return res.send("Membro não existe")
 
-  const instructor = {
-    ...foundInstructor,
-    birth:date(foundInstructor.birth)
+  const member = {
+    ...foundMember,
+    birth:date(foundMember.birth)
   }
 
 
   
 
-  return res.render("instructors/edit", {instructor})
+  return res.render("members/edit", {member})
 }
 
 //Update = put
-
 exports.put = function(req,res) {
   //Buscando dados do corpo do arquivo
   const {id} = req.body
 
   let index = 0
 
-  const foundInstructor = data.instructors.find(function(instructor, foundIndex){
-    instructor.id 
+  const foundMember = data.members.find(function(member, foundIndex){
+    member.id 
 
-    if( id == instructor.id) {
+    if( id == member.id) {
       index = foundIndex
       return true
     }
   })
-  if(!foundInstructor ) return res.send("instrutor não existe")
+  if(!foundMember ) return res.send("Membro não existe")
 
-  const instructor = {
-        ...foundInstructor,
+  const member = {
+        ...foundMember,
         ...req.body,
          birth:Date.parse(req.body.birth),
          id: Number(req.body.id)
   }
 
-  data.instructors[index] = instructor
+  data.members[index] = member
 
   fs.writeFile("data.json", JSON.stringify(data, null,4), function(err){
     if(err) return res.send("write fail erro") 
 
-      return res.redirect(`/instructors/${id}`)
+      return res.redirect(`/members/${id}`)
     
 } )
 
 }
 
-
 //Delete
-
 exports.delete = function(req, res) {
   const {id} = req.body
 
-  const filteredInstructors = data.instructors.filter(function(instructor){
+  const filteredMembers = data.members.filter(function(member){
     //O que significa ser diferente desse id?
-    return instructor.id != id
+    return member.id != id
   })
 
-  data.instructors = filteredInstructors
+  data.members = filteredMembers
 
   fs.writeFile("data.json", JSON.stringify(data, null, 4), function(err) {
     if(err) return res.send("write fail erro")
-    return res.redirect(`/instructors`)
+    return res.redirect(`/members`)
   })
 }
--- routes.js
+++ routes.js
@@ -1,35 +1,39 @@
 const express = require('express')
-
 //Variavel responsavel pelas rotas
 const routes = express.Router()
 //Chamando arquivo instructors com as funções (CRUD)
-const instructors = require('./instructors')
+const instructors = require('./controllers/instructors')
+const members = require('./controllers/members')
+
 
 routes.get('/', function(req, res){
   return res.redirect("/instructors")
 })
 
+
 routes.get('/instructors', instructors.index)
-
-routes.get('/instructors/create', function(req, res){
-  return res.render("instructors/create")
-})
-
+routes.get('/instructors/create', instructors.create)
 routes.get('/instructors/:id/edit', instructors.edit )
-
 routes.get('/instructors/:id', instructors.show)
-
 //Recebendo a requisição via post do corpo
 routes.post('/instructors', instructors.post)
 //Recebendo dados para update
 routes.put('/instructors', instructors.put)
-
 // rota para deletar dados
 routes.delete('/instructors', instructors.delete)
 
-routes.get('/members', function(req, res){
-  return res.send("/members")
-})
+
+routes.get('/members', members.index)
+routes.get('/members/create', members.create)
+routes.get('/members/:id/edit', members.edit )
+routes.get('/members/:id', members.show)
+//Recebendo a requisição via post do corpo
+routes.post('/members', members.post)
+//Recebendo dados para update
+routes.put('/members', members.put)
+// rota para deletar dados
+routes.delete('/members', members.delete)
+
 
 //Exportando o arquivo
 module.exports = routes
